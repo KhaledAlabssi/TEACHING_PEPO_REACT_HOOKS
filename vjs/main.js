473f1479d075a5c951b2e8dada05b58a657404e3
KhaledAlabssi <khaled.absi5@gmail.com>
2024-07-07 12:00:15 +0200

    update vjs starter code

---
+++ vjs/main.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn')
+const cElement = document.getElementById('c');
+let counter = 0;
+
+btn.addEventListener('click', updateC)
+
+function updateC () {
+    counter = counter + 1
+    cElement.innerHTML = counter;
+}
