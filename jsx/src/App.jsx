4fc56bc3d2b8322054252caa7d50262bc5a91136
KhaledAlabssi <khaled.absi5@gmail.com>
2024-07-07 11:35:16 +0200

    update jsx starter code

---
+++ jsx/src/App.jsx
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App dark">
+      <header>
+        <h1>We are the champions!</h1>
+      </header>
+      <main>
+        <h2>
+          React is Fun
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt doloribus fuga consequuntur hic, enim porro sed corrupti nisi rem facilis asperiores, cum magni, blanditiis quibusdam iusto laboriosam voluptas debitis vitae.
+        </p>
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
